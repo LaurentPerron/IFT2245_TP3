0ce4b57ea9d7aa895fd94749eff706145b9949bc
Laurent_P <laurent.perrron@gmail.com>
2020-04-03 11:08:53 -0400

    first commit

---
+++ src/pm.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+
+
+#include "conf.h"
+#include "pm.h"
+
+static FILE *pm_backing_store;
+static FILE *pm_log;
+// Frame 0 : indices 0 ... 255 ; Frame 1 : indices 256 ... 511 ; etc.
+static char pm_memory[PHYSICAL_MEMORY_SIZE]; // 32 frames de tailles 256 char -> 8192 cellules
+static unsigned int download_count = 0;
+static unsigned int backup_count = 0;
+static unsigned int read_count = 0;
+static unsigned int write_count = 0;
+
+// Initialise la mémoire physique
+void pm_init (FILE *backing_store, FILE *log)
+{
+  pm_backing_store = backing_store;
+  pm_log = log;
+  memset (pm_memory, '\0', sizeof (pm_memory));
+}
+
+// Charge la page demandée du backing store
+void pm_download_page (unsigned int page_number, unsigned int frame_number)
+{
+  download_count++;
+
+  int begin = 256 * frame_number;
+  int end = begin + 256;
+  // On amene le cursor a la bonne page
+  fseek(pm_backing_store, page_number * 256, SEEK_SET);
+
+  for(int i = begin; i < end; i++) {
+    pm_memory[i] = (char)fgetc(pm_backing_store);
+  }
+
+  rewind(pm_backing_store);
+  fprintf (pm_log, "%s[%c]@%05d: p=%d, f=%d\n", "DOWNLOADED", page_number, frame_number);
+}
+
+// Sauvegarde la frame spécifiée dans la page du backing store
+void pm_backup_page (unsigned int frame_number, unsigned int page_number)
+{
+  backup_count++;
+
+  int begin = 256 * frame_number;
+  int end = begin + 256;
+  fseek(pm_backing_store, page_number * 256, SEEK_SET);
+
+  for(int i = begin; i < end; i++) {
+    fputc(pm_memory[i], pm_backing_store);
+  }
+
+  rewind(pm_backing_store);
+  fprintf (pm_log, "%s[%c]@%05d: p=%d, f=%d\n", "BACKED-UP", page_number, frame_number);
+}
+
+char pm_read (unsigned int physical_address)
+{
+  read_count++;
+  /* physical address de la forme :
+     XXXXX | XXXXXXXX
+     frame | offset */
+
+  // On cherche le no de frame et le offset
+  int frame_number = physical_address >> 8;
+  int offset = physical_address - (frame_number << 8);
+
+  fprintf (pm_log, "%s[%c]@%05d: pa=%d, f=%d, o=%d\n", "READING", physical_address, frame_number, offset);
+  return pm_memory[frame_number * 256 + offset];
+}
+
+void pm_write (unsigned int physical_address, char c)
+{
+  write_count++;
+  /* physical address de la forme :
+     XXXXX | XXXXXXXX
+     frame | offset */
+
+  // On cherche le no de frame et le offset
+  int frame_number = physical_address >> 8;
+  int offset = physical_address - (frame_number << 8);
+
+  fprintf (pm_log, "%s[%c]@%05d: pa=%d, f=%d, o=%d\n", "READING", physical_address, frame_number, offset);
+  pm_memory[frame_number * 256 + offset] = c;
+}
+
+
+void pm_clean (void)
+{
+  // Enregistre l'état de la mémoire physique.
+  if (pm_log)
+    {
+      for (unsigned int i = 0; i < PHYSICAL_MEMORY_SIZE; i++)
+	{
+	  if (i % 80 == 0)
+	    fprintf (pm_log, "%c\n", pm_memory[i]);
+	  else
+	    fprintf (pm_log, "%c", pm_memory[i]);
+	}
+    }
+  fprintf (stdout, "Page downloads: %2u\n", download_count);
+  fprintf (stdout, "Page backups  : %2u\n", backup_count);
+  fprintf (stdout, "PM reads : %4u\n", read_count);
+  fprintf (stdout, "PM writes: %4u\n", write_count);
+}
---
+++ src/vmm.c
@@ -0,0 +1,123 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "conf.h"
+#include "common.h"
+#include "vmm.h"
+#include "tlb.h"
+#include "pt.h"
+#include "pm.h"
+
+static unsigned int read_count = 0;
+static unsigned int write_count = 0;
+static FILE* vmm_log;
+
+void vmm_init (FILE *log)
+{
+  // Initialise le fichier de journal.
+  vmm_log = log;
+}
+
+
+// NE PAS MODIFIER CETTE FONCTION
+static void vmm_log_command (FILE *out, const char *command,
+                             unsigned int laddress, /* Logical address. */
+		             unsigned int page,
+                             unsigned int frame,
+                             unsigned int offset,
+                             unsigned int paddress, /* Physical address.  */
+		             char c) /* Caractère lu ou écrit.  */
+{
+  if (out)
+    fprintf (out, "%s[%c]@%05d: p=%d, o=%d, f=%d pa=%d\n", command, c, laddress,
+	     page, offset, frame, paddress);
+}
+
+/* Effectue une lecture à l'adresse logique `laddress`.  */
+char vmm_read (unsigned int laddress)
+{
+  int frame_number;
+  int paddress;
+  char c = '!';
+  read_count++;
+
+  int page_number = laddress >> 8;
+  int offset = laddress - (page_number << 8);
+
+  // On cherche dans le tlb
+  if(frame_number = tld_lookup(page_number, 0) > -1) {
+
+    paddress = (frame_number << 8) + offset;
+    c = pm_read(paddress);
+
+  } else { // Si on a un miss on regarde dans le page_table
+
+    if(frame_number = pt_lookup(page_number, 0) > -1) {
+
+      paddress = (frame_number << 8) + offset;
+      c = pm_read(paddress);
+    } else { // Si on a encore un miss, on va chercher directement dans le BACKING_STORE
+
+      // Trouver un frame libre
+      frame_number = download_count % 32;
+
+      pm_download(page_number, frame_number);
+      pt_set_entry(page_number, frame_number, 1);
+      tlb_add_entry(page_number, frame_number, 1);
+
+      paddress = (frame_number << 8) + offset;
+      c = pm_read(paddress);
+    }
+  }
+  vmm_log_command (stdout, "READING", laddress, page_number, frame_number, offset, paddress, c);
+  return c;
+}
+
+/* Effectue une écriture à l'adresse logique `laddress`.  */
+void vmm_write (unsigned int laddress, char c)
+{
+  write_count++;
+  int frame_number;
+  int paddress;
+
+  int page_number = laddress >> 8;
+  int offset = laddress - (page_number << 8);
+
+  // On cherche dans le tlb
+  if(frame_number = tld_lookup(page_number, 1) > -1) {
+
+    paddress = (frame_number << 8) + offset;
+    pm_write(paddress, c);
+
+  } else { // Si on a un miss on regarde dans le page_table
+
+    if(frame_number = pt_lookup(page_number, 1) > -1) {
+
+      paddress = (frame_number << 8) + offset;
+      pm_write(paddress, c);
+    } else { // Si on a encore un miss, on va chercher directement dans le BACKING_STORE
+
+      // Trouver un frame libre
+      frame_number = download_count % 32;
+
+      pm_download(page_number, frame_number);
+      pt_set_entry(page_number, frame_number, 0);
+      tlb_add_entry(page_number, frame_number, 0);
+
+      paddress = (frame_number << 8) + offset;
+      pm_write(paddress, c);
+    }
+  }
+  vmm_log_command (stdout, "WRITING", laddress, page_number, frame_number, offset, paddress, c);
+}
+
+
+// NE PAS MODIFIER CETTE FONCTION
+void vmm_clean (void)
+{
+  fprintf (stdout, "VM reads : %4u\n", read_count);
+  fprintf (stdout, "VM writes: %4u\n", write_count);
+}
